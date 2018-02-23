04b98e146b0cad6d2e0c94ff0e96d8bf56790513
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-02-23 15:54:36 -0500

    Created Tilemap for game

---
+++ Assets/TileMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		public Vector3Int size; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
