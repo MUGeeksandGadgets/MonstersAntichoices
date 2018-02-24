44baa5e7cfd93c5a26d556a31d2f56082982e4ce
CITE <cite@waec3241-02.marshall.edu>
2018-02-24 13:46:54 -0500

    Added Camera Follow

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private GameObject player;
+
+	private Vector3 offset;
+
+	void Start(){
+		player = GameObject.FindWithTag("Player");
+		offset = transform.position - player.transform.position;
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+}
