9a5ccc9ec04874fc0d14d73e830545a97827fc9c
Charlie Murphy <cmsmurp00@gmail.com>
2018-02-23 19:00:25 -0500

    DialogueCanvas added as prefab

--- Assets/Scripts/Dialogue/DialogueDefs.cs
+++ Assets/Scripts/Dialogue/DialogueDefs.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 public class DialogueDefs {
 	public static ConversationNode getConversation(string name) {
 		if (name.Equals("hello world"))
-			return createHelloWorldConversation ();
+			return new Message("Hello world!")
+			         .thenMsg ("Hi?")
+			         .thenMsg ("Who's there?")
+			         .thenMsg ("IT'S PIKACHU")
+			         .getRoot ();
 
 		return null;
-	}
-
-	public static ConversationNode createHelloWorldConversation() {
-		return new Message ("Hello world!")
-			.thenMsg ("Hi?")
-			.thenMsg ("Who's there?")
-			.thenMsg ("IT'S PIKACHU")
-			.getRoot ();
 	}
 }
