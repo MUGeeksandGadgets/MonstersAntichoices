a47048b17ad406108a780d9c024aea95f140f064
Charlie Murphy <cmsmurp00@gmail.com>
2018-02-24 12:27:49 -0500

    White dialogue box

--- Assets/Scripts/Dialogue/DialogueDefs.cs
+++ Assets/Scripts/Dialogue/DialogueDefs.cs
@@ -9,7 +9,8 @@ public class DialogueDefs {
 				.msg ("Oh, my nephew has arrived.")
 				.msg ("Finally!")
 				.msg ("I was wondering,\nyou know?")
-				.msg ("You haven't been doing well in school.")
+				.msg ("You didn't come\nto your cousin's\nbirthday.")
+				.msg ("Indeed. Do try\nto be friends,\nall right?")
 				.msg ("Well, anyway.\nLet's get to it.")
 				.msg ("You just turned 10!\nThat means you need a Nomekop!")
 				.msg ("Come over here to this table.");
